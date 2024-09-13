df0accf2b299eb2ec8a42a30702bf6d4363d0bac
pefish <darrondarron@qq.com>
2024-09-13 23:07:26 +0800

    add

--- packages/cli/src/commands/mint/mint.command.ts
+++ packages/cli/src/commands/mint/mint.command.ts
@@ -140,7 +140,7 @@ export class MintCommand extends BoardcastCommand {
               return;
             }
 
-            if (minterState.remainingSupply < 500) {
+            if (minterState.remainingSupply < 100) {
               console.error(
                 `碎片, remainingSupply: ${minterState.remainingSupply}!`,
               );
